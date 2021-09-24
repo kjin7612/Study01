805d0e456ec233c40222d7f23ef72a5dd8fc0719
Ealry Connect <hyeonjin.kim@earlyconnect.com>
2021-09-23 20:02:10 -0400

    update

--- src/study01/TwoSumII.java
+++ src/study01/TwoSumII.java
@@ -1,5 +1,7 @@
 package study01;
 
+import java.util.Arrays;
+
 /**
  * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= first < second <= numbers.length.
  *
@@ -15,18 +17,33 @@ package study01;
 public class TwoSumII {
     public int[] twoSum(int[] numbers, int target) {
         int[] rtnArr = new int[2];
+
         for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if(i != j && numbers[i] + numbers[j] == target){
+                    System.out.println("i: " + Arrays.asList(numbers[i]) + ", j: " + Arrays.asList(numbers[j]));
+                    rtnArr[0] = i + 1;
+                    rtnArr[1] = j + 1;
+                    System.out.println(Arrays.toString(rtnArr));
+                    return rtnArr;
+                }
+//                if(numbers[j] == 2){
+//                    System.out.println(Arrays.asList(numbers[i]));
+//                    System.out.println(i);
+//                }
+            }
         }
+
         return rtnArr;
     }
 
     public static void main(String[] args) {
         TwoSumII ts = new TwoSumII();
-        int[] numbers = {2, 7, 11, 15};
-        int target = 9;
+//        int[] numbers = {2, 7, 11, 15};
+        int[] numbers = {0, 0, 3, 4};
+        int target = 0;
 
         ts.twoSum(numbers, target);
-
 
     }
 }
